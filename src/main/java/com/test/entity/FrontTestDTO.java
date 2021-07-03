96952b7f1931398cb0e7c173a27be09fd1379353
zengkai <979384911@qq.com>
2021-07-03 15:33:15 +0800

    添加测试前后端联调的dto

---
+++ src/main/java/com/test/entity/FrontTestDTO.java
@@ -0,0 +1,32 @@
+package com.test.entity;
+
+import lombok.Data;
+
+/**
+ * 前端测试dto
+ *
+ * @author zengkai
+ * Date: 2021/7/3 14:14
+ */
+@Data
+public class FrontTestDTO {
+
+    private Byte aByte;
+
+    private Short aShort;
+
+    private Integer integer;
+
+    private Long aLong;
+
+    private Float aFloat;
+
+    private Double aDouble;
+
+    private Boolean aBoolean;
+
+    private Character character;
+
+    private String string;
+
+}
