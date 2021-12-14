3e04394533ceea61450a360eba8e49fed7102096
zengkai <979384911@qq.com>
2021-12-14 17:48:16 +0800

    获取bean缓存池，确定一些Bean是否被加载

--- src/main/java/com/test/impl/CommonServiceImpl.java
+++ src/main/java/com/test/impl/CommonServiceImpl.java
@@ -1,8 +1,15 @@
 package com.test.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import com.myhexin.zixun.config.User;
@@ -26,6 +33,9 @@ public class CommonServiceImpl implements CommonService {
     private UserService userService;
 
     @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
     private PropertyBeanConfig propertyBeanConfig;
 
     public static final Logger logger = LoggerFactory.getLogger(CommonServiceImpl.class);
@@ -46,6 +56,26 @@ public class CommonServiceImpl implements CommonService {
     public String print() {
         logger.info("info: " + user.getId() + " " + user.getName());
         userService.print();
+        AnnotationConfigServletWebServerApplicationContext annotationConfigServletWebApplicationContext =
+                (AnnotationConfigServletWebServerApplicationContext) applicationContext;
+        ConfigurableListableBeanFactory defaultListableBeanFactory =
+                annotationConfigServletWebApplicationContext.getBeanFactory();
+        Boolean contains =
+                defaultListableBeanFactory.containsBeanDefinition("com.myhexin.zixun.config.UserAutoConfiguration");
+        ConcurrentHashMap<String, Object> singletonObjects =
+                (ConcurrentHashMap<String, Object>) defaultListableBeanFactory.getSingletonMutex();
+        Map<String, Object> aimMap = new HashMap<>();
+        for (Map.Entry<String, Object> entry : singletonObjects.entrySet()) {
+            String beanName = entry.getKey();
+            if (beanName.contains("UserService") || beanName.contains("UserAutoConfiguration")
+            || beanName.contains("userService") || beanName.contains("userAutoConfiguration")
+            || beanName.contains("getBean")) {
+                aimMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        Object object = applicationContext.getBean("getBean");
+        Object object2 = applicationContext.getBean("userServiceImpl");
         return user.getName();
     }
 
