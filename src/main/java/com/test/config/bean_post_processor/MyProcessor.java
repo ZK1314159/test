c3c05ae6c5483d1a8a332e1595d2cdc68cf72c77
zengkai <979384911@qq.com>
2022-05-05 17:13:41 +0800

    添加自定义的后处理器

---
+++ src/main/java/com/test/config/bean_post_processor/MyProcessor.java
@@ -0,0 +1,29 @@
+package com.test.config.bean_post_processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import com.test.controller.TestController;
+
+/**
+ * Description
+ *
+ * @author zengkai
+ * Date: 2022/5/5 17:06
+ */
+@Component
+public class MyProcessor implements BeanPostProcessor {
+
+    @Nullable
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        if ("testController".equals(beanName)) {
+            TestController testController = (TestController) bean;
+            testController.setId(7);
+        }
+        return bean;
+    }
+
+}
