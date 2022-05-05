07448701d72c67b12d455f20b6e33144f58bd638
zengkai <979384911@qq.com>
2022-05-05 15:59:36 +0800

    添加filter

---
+++ src/main/java/com/test/config/filter/MyFilter.java
@@ -0,0 +1,29 @@
+package com.test.config.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description
+ *
+ * @author zengkai
+ * Date: 2022/5/5 15:21
+ */
+@Component("myFilter")
+@Slf4j
+public class MyFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("start!!!");
+        chain.doFilter(request, response);
+    }
+}
