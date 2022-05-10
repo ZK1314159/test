7b30ad356bfe21fa18a6c79caf8459c2b675eeeb
zengkai <979384911@qq.com>
2022-05-10 11:57:52 +0800

    拦截器

---
+++ src/main/java/com/test/config/interceptor/MyInterceptor.java
@@ -0,0 +1,12 @@
+package com.test.config.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * Description
+ *
+ * @author zengkai
+ * Date: 2022/5/10 11:15
+ */
+public class MyInterceptor implements HandlerInterceptor {
+}
