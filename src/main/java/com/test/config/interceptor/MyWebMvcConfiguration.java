f431aefd2ae718a21baa9a3448c12a3d1cc58e0f
zengkai <979384911@qq.com>
2022-05-12 21:37:37 +0800

    前端访问后端的静态资源

---
+++ src/main/java/com/test/config/interceptor/MyWebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.test.config.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Description
+ *
+ * @author zengkai
+ * Date: 2022/5/12 21:04
+ */
+@Configuration
+public class MyWebMvcConfiguration implements WebMvcConfigurer {
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("file:D:/Software/Work/Data/Idea/Projects" +
+//                "/Springboot/test/src/main/resources/");
+//    }
+
+}
---
+++ src/main/java/com/test/controller/StaticResourceController.java
@@ -0,0 +1,18 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StaticResourceController {
+
+//    @GetMapping("/front_end/picture.jpg")
+//    public void picture(HttpServletResponse response) throws Exception {
+//        BufferedImage image = ImageIO.read(Files.newInputStream(Paths.get("file:D:\\Software\\Work\\Data\\Idea\\Projects" +
+//                "\\Springboot\\test\\src\\main\\resources\\front_end\\picture.jpg")));
+//        response.setHeader("Cache-Control", "no-store, no-cache");
+//        response.setContentType("image/jpeg");
+//        ServletOutputStream out = response.getOutputStream();
+//        ImageIO.write(image, "jpg", out);
+//    }
+
+}
