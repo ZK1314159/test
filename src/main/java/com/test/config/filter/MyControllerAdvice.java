bc2a6c619044ed2c45c22f5d2f8ffc6494b54c49
zengkai <979384911@qq.com>
2022-05-24 09:43:17 +0800

    controllerAdvice

---
+++ src/main/java/com/test/config/filter/MyControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.test.config.filter;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Description
+ *
+ * @author zengkai
+ * Date: 2022/5/23 22:23
+ */
+@ControllerAdvice
+public class MyControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ModelAndView runtimeException(RuntimeException e) {
+        e.printStackTrace();
+        return new ModelAndView("error");
+    }
+
+}
